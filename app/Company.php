ae5da5b8c22c0753fde969df82cd66d4e62090be
niypoo <niypoo@gmail.com>
2020-04-29 18:19:47 +0200

    fix journals issue merge

--- app/Company.php
+++ app/Company.php
@@ -8,7 +8,7 @@ class Company extends Model
 {
 
     public $translatable = ['name'];
-    public $with = ['branches', 'Currencies', 'projects', 'employees'];
+    public $with = ['branches', 'Currencies', 'projects', 'employees', 'Accounts', 'journals'];
 
     protected $fillable = [
         'id',
